63ad2dd357f0c166800c26972c47238a79512ed7
Chong5084 <dmschd9647@gmail.com>
2023-06-27 13:13:11 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# $ pip install streamlit
+# $ streamlit hello # $ = 터미널에 쓰인다는 의미
+# ctrl + c : 실행 종료 (터미널)
+#  streamlit run main.py
+
+
+import streamlit as st
+
+
+
+
+st.title("Air pollution in Seoul")
+st.image("https://img.freepik.com/free-photo/view-of-metro-city-buildings-cityscape_53876-14497.jpg?size=626&ext=jpg")
+st.image("https://img.freepik.com/free-photo/view-of-metro-city-buildings-cityscape_53876-14497.jpg?size=626&ext=jpg")
+
+
